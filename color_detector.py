52e19d4347c19623f0d74621f81b3d9536d4f93e
jonathan-peel <jonathan.n.peel@gmail.com>
2020-10-20 18:10:41 +0200

    N_SPLITS environment variable input successful

--- color_detector.py
+++ color_detector.py
@@ -1,20 +1,31 @@
 #!/usr/bin/env python3
 
 import numpy as np
+import os
 import cv2
 from time import sleep
 
+# set cap to continuously capture images from the camera
 cap = cv2.VideoCapture(2)
+
+# Find number of splits input at runtime
+n_splits = os.environ["N_SPLITS"]
+# Environment variables are returned as strings
+try:
+    n_splits = int(n_splits) # Rounds to the nearest integer
+except ValueError:
+    print("Please input an integer as -e N_SPLITS=<integer>, defaulting to N_SPLITS=1")
+    n_splits = 1
+    
 i = 0
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
-    #Put here your code!
-    # You can now treat output as a normal numpy array
-    # Do your magic here
+    # frame variable is the camera image as a numpy array
+    # ret is true if the frame was captured sucessfully and false otherwise
 
     sleep(1)
     i += 1
-    print("iteration" + str(i))
+    print("Frame " + str(i))
